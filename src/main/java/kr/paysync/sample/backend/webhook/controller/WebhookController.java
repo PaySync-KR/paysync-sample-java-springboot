c43b64a5550fb1b0294f0f2a14734b0a907be54f
eunhyun05 <eunhyun050812@gmail.com>
2026-03-09 19:21:11 +0900

    fix: Map<String, String> -> HttpHeaders 타입 변경

--- src/main/java/kr/paysync/sample/backend/webhook/controller/WebhookController.java
+++ src/main/java/kr/paysync/sample/backend/webhook/controller/WebhookController.java
@@ -3,10 +3,10 @@ package kr.paysync.sample.backend.webhook.controller;
 import kr.paysync.sample.backend.webhook.service.WebhookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -20,18 +20,19 @@ public class WebhookController {
 
     @PostMapping
     public ResponseEntity<Void> handleWebhook(
-            @RequestHeader Map<String, String> headers,
+            @RequestHeader HttpHeaders headers,
             @RequestBody String rawBody
     ) {
         service.handleWebhook(
-                headers.entrySet()
+                headers.headerSet()
                         .stream()
                         .collect(Collectors.toMap(
                                 Map.Entry::getKey,
-                                e -> List.of(e.getValue())
+                                Map.Entry::getValue
                         )),
                 rawBody
         );
+
         return ResponseEntity.ok().build();
     }
 }
