29eab6aec45bfdb86df103148f36a5bd25772b10
NillPoe <nillpoe@pm.me>
2026-03-09 15:08:20 +0900

    refactor: remove redundant annotations.

--- src/main/java/kr/paysync/sample/backend/bankaccount/enums/BankAccountType.java
+++ src/main/java/kr/paysync/sample/backend/bankaccount/enums/BankAccountType.java
@@ -1,10 +1,5 @@
 package kr.paysync.sample.backend.bankaccount.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum BankAccountType {
 
     PERSONAL,  // 개인
